4fa62541fd1620bf3ccdc1ff5133c72463d5ab7f
马贤飞 <simple_95@163.com>
2022-02-11 16:12:46 +0800

    删除依赖

--- example/app.module.ts
+++ example/app.module.ts
@@ -1,5 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { GraphQLModule } from '@nestjs/graphql';
 
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
